3827c21909905e5e8dfd2c9e9f97fb9ac55ae553
Alesh Houdek <alesh.houdek@sovos.com>
2021-01-30 12:57:26 -0700

    re-do tutorial

--- polls/admin.py
+++ polls/admin.py
@@ -3,17 +3,17 @@ from django.contrib import admin
 from .models import Question, Choice
 
 class ChoiceInline(admin.TabularInline):
-  model = Choice
-  extra = 0
+    model = Choice
+    extra = 3
 
 class QuestionAdmin(admin.ModelAdmin):
-  fieldsets = [
-    (None, {'fields': ['question_text']}),
-    ('Date information', {'fields': ['pub_date']}),
-  ]
-  inlines = [ChoiceInline]
-  list_display = ('question_text', 'pub_date', 'was_published_recently')
-  list_filter = ['pub_date']
-  search_fields = ['question_text']
+    search_fields = ['question_text']
+    list_filter = ['pub_date']
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
 
 admin.site.register(Question, QuestionAdmin)
--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
 from django.utils import timezone
@@ -7,40 +7,40 @@ from django.utils import timezone
 from .models import Question, Choice
 
 class IndexView(generic.ListView):
-  template_name = 'polls/index.html'
-  context_object_name = 'latest_question_list'
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
-  def get_queryset(self):
-    """
-    Return the last five published questions, not including those set to
-    be published in the future.
-    """
-    return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
+    def get_queryset(self):
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
+        # return Question.objects.order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
-  model = Question
-  template_name = 'polls/detail.html'
+    model = Question
+    template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        """
+        Excludes and questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 class ResultsView(generic.DetailView):
-  model = Question
-  template_name = 'polls/results.html'
+    model = Question
+    template_name = 'polls/results.html'
 
 def vote(request, question_id):
-  question = get_object_or_404(Question, pk=question_id)
-  choice_id = request.POST['choice']
-  try:
-    selected_choice = question.choice_set.get(pk=choice_id)
-  except (KeyError, Choice.DoesNotExist):
-    # Redisplay the question voting form.
-    error_message = "You didn't select a choice. %s" % choice_id
-    return render(request, 'polls/detail.html', {
-      'question': question,
-      'error_message': error_message,
-    })
-  else:
-    selected_choice.votes += 1
-    selected_choice.save()
-    # Always return an HttpResponseRedirect after successfully dealing
-    # with POST data. This prevents data from being posted twice if a
-    # user hits the Back button.
-    return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
